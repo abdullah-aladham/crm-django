ef87f7da855a1ed23a6d2603df454ce4ef304efe
Abdullah Aladham <abdullah.aladham@gmail.com>
2021-11-27 23:26:52 +0200

    after deadline working status

--- customer/models.py
+++ customer/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.db.models.fields import AutoField
+from service.models import service
 
 # Create your models here.
 class customer(models.Model):
     id=models.IntegerField(primary_key=AutoField)
     name=models.CharField(max_length=50)
+    services_name=models.ManyToManyField(service)
    # email=models.EmailField( max_length=254)
     #email=models.EmailField((""), max_length=254)
    # phoneno=models.PhoneNumberField()
-    service_id=models.ForeignKey
+   # service_id=models.ForeignKey(service,on_delete=models.CASCADE)
 
     #we need services col which is joined 
+    
 
 
--- service/models.py
+++ service/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from customer.models import customer
+#from customer.models import customer
 
 
 # Create your models here.
@@ -10,7 +10,5 @@ class service(models.Model):
     ser_id=models.IntegerField()
     Customer=models.ForeignKey
     amount=models.IntegerField()
-    #customer_id=models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
-    
-
+    #customer_id= models.ManyToManyField(customer)
     #we need services col which is joined 
